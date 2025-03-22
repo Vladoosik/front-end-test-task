57eb9652e5f93e4483192b62be7109d9747e0c6e
vladoosik <x.vlad2101@gmail.com>
2025-03-23 01:25:28 +0200

    refactoring

--- src/services/catsService.ts
+++ src/services/catsService.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { CatModel } from "../../types/catModel.ts";
+
 export const catsApi = createApi({
   reducerPath: "catsApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.thecatapi.com/v1",
   }),
   endpoints: (builder: any) => ({
-    getBreeds: builder.query<CatModel[], void>({
+    getBreeds: builder.query({
       query: () => "/breeds",
     }),
   }),
